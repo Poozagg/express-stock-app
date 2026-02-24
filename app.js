d7f8ab3ce22647c4174d1765745c91a2b07dccb9
Pooja Gurung <147177106+Poozagg@users.noreply.github.com>
2026-02-24 10:27:16 +0000

    created routes and controller

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const productRoutes = require('./routes/productRoutes');
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('./public'));
+
+// Use product routes
+app.use('/', productRoutes);
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+    console.log(`Visit http://localhost:${port}`);  
+});
